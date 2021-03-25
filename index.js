87add1d0407135451880215460993e3906b4da76
Max Roche <maxproche@gmail.com>
2021-03-25 17:08:07 -0500

    Adds initial bitcoin amounts

---
+++ index.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", fetchBitcoinPrice)
+
+let initialPurchasePrice = 51448.44
+let bitcoinOwned = 0.045964
+
+let url = "https://api.coindesk.com/v1/bpi/currentprice/BTC.json"
+
+function fetchBitcoinPrice() {
+  fetch(url)
+    .then( (response) => response.json() )
+    .then( (jsonData) => jsonData["bpi"] )
+    .then( (bpiData) =>  bpiData["USD"] )
+    .then( usdData => usdData.rate )
+    .then( rateString => {
+      let rateWithoutCommas = rateString.replace(",", "")
+      let rateValue = parseFloat(rateWithoutCommas)
+      let currentPrice = bitcoinOwned * rateValue
+
+      let isGain = currentPrice - 1200 > 0
+      let styleColor = isGain ? "green" : "red"
+
+      let element = document.getElementById("gr-pot-price")
+      element.innerHTML = "$" + currentPrice
+      element.style.color = styleColor
+    })
+}
